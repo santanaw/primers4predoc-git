47717e0fe5180955594b4ee187a591da6ddc8f19
santanaw <wsantana@ebi.ac.uk>
2023-01-18 15:39:23 +0000

    Standard fixed #13

--- src/script_issue_13.py
+++ src/script_issue_13.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-# Complete the operation below, adding a single arithmetic operator  so 'result' can be equal to '26'
+#   so 'result' can be equal to '26'
 RESULT = 13 + 13
 
 # Print output
